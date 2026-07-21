43c746acf43d457e14749d9902c3f448b40f6f6c
5406_MUHAMMAD ARFIAN <arfian23@students.amikom.ac.id>
2026-07-21 18:32:26 +0700

    Fix CORS syntax

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://URL-VERCEL-ANDA.vercel.app",
+        "uas-pdm-production.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
